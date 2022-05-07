7b0868b442aaed45ac79aa1e3e7aeda1d7eeb2d0
limonzishad <zishadlimon@gmail.com>
2022-05-07 14:43:25 +0600

    add item added

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ async function run() {
         await client.connect();
         const itemCollection = client.db('handset-store').collection('item');
 
-        app.get('/item', async (req, res) => {
+        app.get('/items', async (req, res) => {
             const query = {};
             const cursor = itemCollection.find(query);
             const items = await cursor.toArray();
@@ -27,13 +27,22 @@ async function run() {
 
         });
 
+        //show specific item
         app.get('/item/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const item = await itemCollection.findOne(query);
             res.send(item);
         });
+
+        //add new item
+        app.post('/item', async (req, res) => {
+            const newItem = req.body;
+            const result = await itemCollection.insertOne(newItem);
+            res.send(result);
+        });
     }
+
     finally {
 
     }
